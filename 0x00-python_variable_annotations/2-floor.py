8aa89d67650c5eba01f0a68f8727a0f923be78f5
NenyeIkpa <nenyeikpa@gmail.com>
2024-05-01 09:01:33 -0700

    length

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for task 2
+"""
+
+
+def floor(a: float) -> int:
+    """Type-annotated function which returns the floor of a float
+
+    Args:
+        a (float): float to floor.
+
+    Returns:
+        int: The floor of the float.
+    """
+    return int(a)
