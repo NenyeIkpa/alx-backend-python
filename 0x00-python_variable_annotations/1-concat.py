c6727efeb56f801a510abfc8963904c4e5ab147e
NenyeIkpa <nenyeikpa@gmail.com>
2024-05-01 09:15:33 -0700

    concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""Module for task 1
+"""
+Module for task 1
 """
 
 
 def concat(str1: str, str2: str) -> str:
-    """Type-annotated function that concatenates two strings
-    
+    """Type-annotated function that takes a string str1 and a string
+    str2 as arguments and returns a concatenated string.
+
     Args:
         str1 (str): first string to concatenate.
         str2 (str): second string to concatenate.
